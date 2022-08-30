ff94bf447b7387cb745e198b81422399689f1bae
czavate <czavate>
2022-08-30 13:07:51 +0000

    Modified cmd 1

--- Example/1. Notebooks (1).py
+++ Example/1. Notebooks (1).py
@@ -5,6 +5,7 @@
 # MAGIC * Run a notebook from another notebook by using the %run < notebook > magic command
 # MAGIC * All variables defined in < notebook > become available in your current notebook
 # MAGIC * After you attach a notebook to a cluster and run one or more cells, your notebook has state and displays results
+# MAGIC * Test
 # MAGIC * Databricks has basic version control for notebooks
 
 # COMMAND ----------
