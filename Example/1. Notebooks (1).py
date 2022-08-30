1e50e940374d3fa376bced3eebaa1b604b9e820c
czavate <czavate>
2022-08-30 12:55:44 +0000

    Added New Example

---
+++ Example/1. Notebooks (1).py
@@ -0,0 +1,27 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC * Collection of runnable cells (commands)
+# MAGIC * Mix programming languages by using %< language > at the beginning of a cell
+# MAGIC * Run a notebook from another notebook by using the %run < notebook > magic command
+# MAGIC * All variables defined in < notebook > become available in your current notebook
+# MAGIC * After you attach a notebook to a cluster and run one or more cells, your notebook has state and displays results
+# MAGIC * Databricks has basic version control for notebooks
+
+# COMMAND ----------
+
+dbutils.notebook.run("./1. Pre-requisites", 60)
+
+# COMMAND ----------
+
+# MAGIC %py
+# MAGIC myFirstDF.display()
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC spark.sql("select * from number_table").display()
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from number_table
